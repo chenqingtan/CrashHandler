a29ef3024fd11f9101f9678ce46a6af03514f7f2
codingfun <kailaniyung@outlook.com>
2016-05-09 21:21:37 +0800

    specify the listening port when app starts,different color for msg respectively

--- CrashHandlerServer/src/Server.java
+++ CrashHandlerServer/src/Server.java
@@ -8,12 +8,28 @@ import com.fireofandroid.crashhandler.CrashMsg;
 
 public class Server {
  
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	
    public static void main(String args[]) throws IOException {
-     int port = 8899;
+	 if (args.length != 1) {
+		 System.out.println("参数非法, 使用示例(java Server port):" + 
+				 "\n\t\\java Server 8899");
+		 
+		 return;
+	 }
+		 
+     int port = Integer.parseInt(args[0]);
      
      //定义一个ServerSocket监听在端口8899上
      ServerSocket server = new ServerSocket(port);
-     System.out.println("创建监听端口成功");
+     System.out.println("创建监听端口:" + port + "成功");
      while (true) {
     	 System.out.println("等待连接...");
          //server尝试接收其他Socket的连接请求，server的accept方法是阻塞式的
@@ -54,14 +70,25 @@ public class Server {
 	    	 ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
 	    	 CrashMsg msg = (CrashMsg) ois.readObject();
 	    	 ois.close();
-	    	 String traceup = msg.getStackTraceUp();
-	         System.out.println("From IP:" + socket.getInetAddress().getHostName() + 
+	    	 String color = ANSI_WHITE;
+	    	 switch (msg.getType()) {
+	    	 case 'E':
+	    		 color = ANSI_RED;
+	    		 break;
+	    	 case 'M':
+	    		 color = ANSI_WHITE;
+	    		 break;
+	    	 default:
+	    			 break; 			
+	    	 }
+	    	 
+	         System.out.println(color + "From IP:" + socket.getInetAddress().getHostName() + 
 	        		 "---" 
 	        		 + msg.getDate() + "-"
 	        		 + msg.getType() + "-"
 	        		 + msg.getThread() + "-"
 	        		 + msg.getBody() + "-"
-	        		 + traceup);
+	        		 + msg.getStackTraceUp() + ANSI_RESET);
 	         socket.close();
     	 } catch (IOException e) {
     		 e.printStackTrace();
